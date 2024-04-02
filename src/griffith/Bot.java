886ac2587cbe2fa03244101e34aa06ff92231550
ruishen99 <ruishen1102@gmail.com>
2024-04-02 20:44:47 +0100

    Add temp attribute and constructor to Bot class

--- src/griffith/Bot.java
+++ src/griffith/Bot.java
@@ -1,5 +1,22 @@
 package griffith;
 
 public class Bot {
+	//Attribute
+	private double temp;
+	
+	//constructor
+	public Bot(double temp) {
+		this.setTemp(temp);
+	}
 
+	//Getter and Setter
+	public double getTemp() {
+		return temp;
+	}
+
+	public void setTemp(double temp) {
+		this.temp = temp;
+	}
+	
+	
 }
