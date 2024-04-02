fdd34548d86efe327aadf35ceb9386a17ab14c62
ruishen99 <ruishen1102@gmail.com>
2024-04-02 20:50:34 +0100

    Update testClothesSuggestion so it test with Bot class

--- src/TestMain/TestMain.java
+++ src/TestMain/TestMain.java
@@ -4,35 +4,37 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import griffith.Bot;
 import griffith.Main;
 
 class TestMain {
 
 	@Test
 	void testClothesSuggestion() {
-		Main main = new Main();
+		//Create Bot object with initialize temp as 0
+		Bot bot = new Bot(0);
 		
 		//Test method with different numbers
-		String output = main.clothesSuggestion(10);
+		String output = bot.clothesSuggestion(10);
 		assertEquals("Wear jacket", output);
 		
-		String output2 = main.clothesSuggestion(26);
+		String output2 = bot.clothesSuggestion(26);
 		assertEquals("Wear t-shirt", output2);
 		
 		//Test method with negative number
-		String output3 = main.clothesSuggestion(-5);
+		String output3 = bot.clothesSuggestion(-5);
 		assertEquals("Wear jacket and sweater", output3);
 		
 		//Test method with extreme negative number
-		String output4 = main.clothesSuggestion(-50);
+		String output4 = bot.clothesSuggestion(-50);
 		assertEquals("Too cold, don't go outside", output4);
 		
 		//Test method with decimal number
-		String output5 = main.clothesSuggestion(14.55);
+		String output5 = bot.clothesSuggestion(14.55);
 		assertEquals("Wear hoodie", output5);
 		
 		//Test method with extreme positive number
-		String output6 = main.clothesSuggestion(80);
+		String output6 = bot.clothesSuggestion(80);
 		assertEquals("Too hot, don't go outside", output6);
 
 		
