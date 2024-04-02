88c780a0770480be6444581ea884bfdc44d7e008
ruishen99 <ruishen1102@gmail.com>
2024-04-02 19:52:39 +0100

    Add clothesSuggestion method stubs in Main Class

--- src/griffith/Main.java
+++ src/griffith/Main.java
@@ -25,5 +25,7 @@ public class Main {
         System.out.println("Temp: "+cwd.getMainData().getTemp());
     }	
 	
-	
+	public String clothesSuggestion(String city) {
+        return null;
+    }
 }
