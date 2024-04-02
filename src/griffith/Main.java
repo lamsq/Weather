81fcc14258843e265384d297665db11e63509095
KIRILL SMIRNOV <kirill.smirnov@student.griffith.ie>
2024-04-02 01:22:35 +0100

    kelvinToCelsius method deleted, test class deleted

--- src/griffith/Main.java
+++ src/griffith/Main.java
@@ -2,6 +2,7 @@ package griffith;
 
 import net.aksingh.owmjapis.api.APIException;
 import net.aksingh.owmjapis.core.OWM;
+import net.aksingh.owmjapis.core.OWM.Unit;
 import net.aksingh.owmjapis.model.CurrentWeather;
 
 public class Main {
@@ -10,23 +11,18 @@ public class Main {
             
         // declaring object of "OWM" class with the API key
         OWM owm = new OWM("bd1e2a9675bcd866cce494364b798612");
-
+        owm.setUnit(Unit.METRIC);
+        
         // getting current weather data for the chosen city
         CurrentWeather cwd = owm.currentWeatherByCityName("Berlin");
+        
 
         //printing city name from the retrieved data
         System.out.println("City: " + cwd.getCityName());
         
-        // printing the max./min. temperature
-        System.out.println("Temperature: " + cwd.getMainData().getTempMax() + "/" + cwd.getMainData().getTempMin() + "\'K");
-        
-    }
-	
-	//converts the temperature from K to C	
-	public static double kelvinToCelcius(double kelvin) { 
-		return Math.round((kelvin-273.15)*100)/100; //returns the converted value rounded to 2 decimals
-	}
-	
+        //Actual temperature
+        System.out.println("Temp: "+cwd.getMainData().getTemp());
+    }	
 	
 
 }
