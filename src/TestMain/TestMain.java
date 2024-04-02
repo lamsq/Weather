d3ae3e391775ae4d223ae6dbdce964d66eea5e12
ruishen99 <ruishen1102@gmail.com>
2024-04-02 20:05:47 +0100

    Implement testClothesSuggestion

--- src/TestMain/TestMain.java
+++ src/TestMain/TestMain.java
@@ -4,11 +4,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import griffith.Main;
+
 class TestMain {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testClothesSuggestion() {
+		Main main = new Main();
+		
+		String output = main.clothesSuggestion(10);
+		assertEquals("Wear jacket", output);
+		
+		String output2 = main.clothesSuggestion(26);
+		assertEquals("Wear t-shirt", output2);
+		
+		String output3 = main.clothesSuggestion(-5);
+		assertEquals("Wear jacket and sweater", output3);
 	}
 
 }
